d0709a1533fedbb0806310f4b1252e161baee8fc
Reina <20066312@tafe.wa.edu.au>
2025-09-28 15:42:20 +0800

    Feat(Game Manager): Add GameManager class - handles gameplay logic - simulates round of combat - allows plays to add items to inventory

---
+++ src/game_manager.py
@@ -0,0 +1,11 @@
+class GameManager:
+    def __init__(self):
+        self.players = []
+        self.npcs = []
+        self.enemies = []
+
+    def make_action(self):
+        ...
+
+    def do_round(self):
+        ...
